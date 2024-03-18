b39393b01618d9c36a074abc8e50a034183225ab
DanielT08 <150853060+DanielT08@users.noreply.github.com>
2024-03-18 15:12:39 -0500

    Agregando ejercicios

---
+++ index.js
@@ -0,0 +1,556 @@
+db.createCollection("users");
+
+// Insertar usuarios a la coleccion de uno en uno
+db.users.insertOne({
+  name: "Daniel",
+  Lastname: "Toro",
+  email: "daniel123@gmail.com",
+  city: "medellin",
+  country: "colombia",
+  salary: 1000,
+  age: 30,
+  height: 170,
+  weight: 160,
+});
+
+db.users.insertOne({
+  name: "Samuel",
+  Lastname: "Mendoza",
+  email: "samuel123@gmail.com",
+  city: "medellin",
+  country: "colombia",
+  salary: 2000,
+  age: 18,
+  height: 160,
+  weight: 100,
+});
+
+db.users.insertOne({
+  name: "Luisa",
+  Lastname: "Marquez",
+  email: "luisa123@gmail.com",
+  city: "paris",
+  country: "francia",
+  salary: 3000,
+  age: 25,
+  height: 160,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "Fernando",
+  Lastname: "Londoño",
+  email: "fernando123spam@gmail.com",
+  city: "londres",
+  country: "inglaterra",
+  salary: 4000,
+  age: 30,
+  height: 170,
+  weight: 160,
+});
+
+db.users.insertOne({
+  name: "luz",
+  Lastname: "Londoño",
+  email: "luz123@gmail.com",
+  city: "madrid",
+  country: "españa",
+  salary: 3500,
+  age: 55,
+  height: 150,
+  weight: 100,
+});
+
+db.users.insertOne({
+  name: "Dania",
+  Lastname: "Toro",
+  email: "dania123@gmail.com",
+  city: "montreal",
+  country: "canada",
+  salary: 2000,
+  age: 26,
+  height: 170,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "David",
+  Lastname: "Toro",
+  email: "david123spam@gmail.com",
+  city: "new york",
+  country: "estados unidos",
+  salary: 3000,
+  age: 27,
+  height: 180,
+  weight: 130,
+});
+
+db.users.insertOne({
+  name: "Dario",
+  Lastname: "Gomez",
+  email: "dario123@gmail.com",
+  city: "londres",
+  country: "inglaterra",
+  salary: 3000,
+  age: 31,
+  height: 170,
+  weight: 160,
+});
+
+db.users.insertOne({
+  name: "Diomedes",
+  Lastname: "Diaz",
+  email: "diomedes123@gmail.com",
+  city: "sidney",
+  country: "australia",
+  salary: 3000,
+  age: 29,
+  height: 150,
+  weight: 150,
+});
+
+db.users.insertOne({
+  name: "Raul",
+  Lastname: "Reyes",
+  email: "raul123@gmail.com",
+  city: "ontario",
+  country: "canada",
+  salary: 1500,
+  age: 41,
+  height: 140,
+  weight: 90,
+});
+
+db.users.insertOne({
+  name: "Carlos",
+  Lastname: "Lether",
+  email: "carlos123@gmail.com",
+  city: "quebed",
+  country: "canada",
+  salary: 5000,
+  age: 37,
+  height: 190,
+  weight: 190,
+});
+
+db.users.insertOne({
+  name: "Santiago",
+  Lastname: "Giraldo",
+  email: "santiago123@gmail.com",
+  city: "roma",
+  country: "italia",
+  salary: 3000,
+  age: 28,
+  height: 170,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "Felipe",
+  Lastname: "Gridales",
+  city: "lima",
+  country: "peru",
+  salary: 2000,
+  age: 20,
+  height: 160,
+  weight: 100,
+});
+
+db.users.insertOne({
+  name: "Daniel",
+  Lastname: "Montiel",
+  email: "danie123@gmail.com",
+  city: "paris",
+  country: "francia",
+  salary: 4000,
+  age: 28,
+  height: 170,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "Alberto",
+  Lastname: "Montiel",
+  email: "alberto123@gmail.com",
+  city: "rio",
+  country: "brasil",
+  salary: 3000,
+  age: 30,
+  height: 180,
+  weight: 150,
+});
+
+db.users.insertOne({
+  name: "Jose",
+  Lastname: "Acevedo",
+  email: "jose123@gmail.com",
+  city: "santiago",
+  country: "chile",
+  salary: 2000,
+  age: 21,
+  height: 160,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "Pedro",
+  Lastname: "Restrepo",
+  email: "pedro123@gmail.com",
+  city: "buenos aires",
+  country: "argentina",
+  salary: 3000,
+  age: 20,
+  height: 160,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "carlos",
+  Lastname: "Restrepo",
+  email: "carlosr123@gmail.com",
+  city: "barselona",
+  country: "españa",
+  salary: 2000,
+  age: 25,
+  height: 170,
+  weight: 130,
+});
+
+db.users.insertOne({
+  name: "Pipe",
+  Lastname: "Rojas",
+  email: "pipe123@gmail.com",
+  city: "ciudad de mexico",
+  country: "mexico",
+  salary: 2500,
+  age: 30,
+  height: 180,
+  weight: 100,
+});
+
+db.users.insertOne({
+  name: "Estefania",
+  Lastname: "Guerra",
+  email: "estefania123@gmail.com",
+  city: "berlin",
+  country: "alemania",
+  salary: 3000,
+  age: 40,
+  height: 150,
+  weight: 110,
+});
+
+db.users.insertOne({
+  name: "Aurora",
+  Lastname: "Espitia",
+  email: "estefania123@gmail.com",
+  city: "hamburgo",
+  country: "alemania",
+  salary: 3500,
+  age: 37,
+  height: 150,
+  weight: 110,
+});
+
+db.users.insertOne({
+  name: "Charrito",
+  Lastname: "Negro",
+  city: "medellin",
+  country: "colombia",
+  salary: 2000,
+  age: 31,
+  height: 160,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "Bahala",
+  Lastname: "Guantanamera",
+  email: "bahala123@gmail.com",
+  city: "jaipur",
+  country: "india",
+  age: 23,
+  height: 180,
+  weight: 110,
+});
+
+db.users.insertOne({
+  name: "Neguid",
+  Lastname: "Fadul",
+  email: "neguid123@gmail.com",
+  city: "jaipur",
+  country: "india",
+  age: 32,
+  height: 170,
+  weight: 210,
+});
+
+db.users.insertOne({
+  name: "Santiago",
+  Lastname: "Mona",
+  email: "santiagomona123@gmail.com",
+  city: "new york",
+  country: "estados unidos",
+  salary: 6000,
+  age: 30,
+  height: 170,
+  weight: 160,
+});
+
+db.users.insertOne({
+  name: "Maria",
+  Lastname: "Majarrez",
+  email: "maria123@gmail.com",
+  city: "pasto",
+  country: "colombia",
+  salary: 2000,
+  age: 22,
+  height: 150,
+  weight: 100,
+});
+
+db.users.insertOne({
+  name: "Cristina",
+  Lastname: "Gomez",
+  email: "cristina123@gmail.com",
+  city: "bogota",
+  country: "colombia",
+  salary: 2500,
+  age: 55,
+  height: 160,
+  weight: 110,
+});
+
+db.users.insertOne({
+  name: "Luisa",
+  Lastname: "Quiroz",
+  email: "luisa123@gmail.com",
+  city: "medellin",
+  country: "colombia",
+  salary: 6000,
+  age: 28,
+  height: 150,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "Rigo",
+  Lastname: "Uran",
+  email: "rigo123@gmail.com",
+  city: "london",
+  country: "inglaterra",
+  salary: 4000,
+  age: 38,
+  height: 170,
+  weight: 100,
+});
+
+db.users.insertOne({
+  name: "John",
+  Lastname: "Doe",
+  email: "john123@gmail.com",
+  city: "paris",
+  country: "francia",
+  salary: 5000,
+  age: 18,
+  height: 150,
+  weight: 100,
+});
+
+db.users.insertOne({
+  name: "Dora",
+  Lastname: "Exploradora",
+  city: "tokyo",
+  country: "japon",
+  salary: 1000,
+  age: 55,
+  height: 150,
+  weight: 100,
+});
+
+db.users.insertOne({
+  name: "Valentina",
+  Lastname: "Toro",
+  city: "orlando",
+  country: "estados unidos",
+  salary: 2000,
+  age: 15,
+  height: 170,
+  weight: 120,
+});
+
+db.users.insertOne({
+  name: "Yonatan",
+  Lastname: "Marquez",
+  city: "california",
+  country: "estados unidos",
+  salary: 0,
+  age: 25,
+  height: 180,
+  weight: 130,
+});
+
+db.users.insertOne({
+  name: "Valentina",
+  Lastname: "Betancourt",
+  email: "valentina123@gmail.com",
+  city: "bello",
+  country: "colombia",
+  salary: 5500,
+  age: 53,
+  height: 190,
+  weight: 140,
+});
+
+db.users.insertOne({
+  name: "Laura",
+  Lastname: "Cortez",
+  email: "laura123@gmail.com",
+  city: "armenia",
+  country: "colombia",
+  salary: 3500,
+  age: 78,
+  height: 150,
+  weight: 100,
+});
+
+db.users.insertOne({
+  name: "Lorena",
+  Lastname: "Montes",
+  email: "lorena123@gmail.com",
+  city: "lima",
+  country: "peru",
+  salary: 3500,
+  age: 36,
+  height: 200,
+  weight: 150,
+});
+
+db.users.find();
+
+db.users.drop();
+
+//1 Incrementar el salario de todos los usuarios en un 10 %.
+db.users.updateMany({}, { $mul: { salary: 1.1 } });
+
+//2 Cambiar la ciudad de residencia de los usuarios que actualmente viven en "New York" a "Los Ángeles".
+db.users.updateMany({ city: "new york" }, { $set: { city: "los angeles" } });
+
+//3 Agregar el correo electrónico "nuevo@correo.com" al usuario con nombre "Juan" y apellido "Perez".
+db.users.updateMany({ name: "Samuel", Lastname: "Mendoza" }, { $set: { email: "samuelmendoza@gmail.com" } });
+
+//4 Actualizar la edad del usuario con correo electrónico "ejemplo@correo.com" a 35 años.
+db.users.updateMany({ email: "daniel123@gmail.com" }, { $set: { age: 35 } });
+
+//5 Cambiar el país de residencia de los usuarios que son de "Mexico" a "Canada".
+db.users.updateMany({ country: "mexico" }, { $set: { country: "canada" } });
+
+//6 Aumentar la altura de todos los usuarios en 5 centímetros.
+db.users.updateMany({}, { $inc: { height: 5 } });
+
+//7 Cambiar  el apellido del usuario con correo electrónico "otro@ejemplo.com" a "González".
+db.users.updateMany({ email: "carlos123@gmail.com" }, { $set: { Lastname: "Gonzalez" } });
+
+//8 Actualizar el peso del usuario con nombre "Maria" a 140 libras.
+db.users.updateMany({ name: "Maria" }, { $set: { weight: 140 } });
+
+//9 Incrementar el salario de todos los usuarios que son de "Estados Unidos" en un 15%.
+db.users.updateMany({ country: "estados unidos" }, { $mul: { salary: 1.15 } });
+
+//10 Actualizar el correo electrónico del usuario con nombre "Pedro" a "nuevo_correo@riwi.co".
+db.users.updateMany({ name: "Pedro" }, { $set: { email: "pedronuevo@riwi.co" } });
+
+//11 Cambiar  la edad de todos los usuarios menores de 30 años a 30 años.
+db.users.updateMany({ age: { $lt: 30 } }, { $set: { age: 30 } });
+
+//12 Incrementar el salario de los usuarios que tienen un salario inferior a 3000 dólares en un 20%.
+db.users.updateMany({ salary: { $lt: 3000 } }, { $mul: { salary: 1.2 } });
+
+//13 Actualizar la ciudad de residencia de todos los usuarios que actualmente viven en "Bogotá" a "Medellín".
+db.users.updateMany({ city: "bogota" }, { $set: { city: "medellin" } });
+
+//14 Cambiar  el país de residencia de los usuarios con un salario superior a 5000 dólares a "Australia".
+db.users.updateMany({ salary: { $gt: 5000 } }, { $set: { country: "australia" } });
+
+//15 Reducir la edad de todos los usuarios que tienen más de 50 años en 5 años.
+db.users.updateMany({ age: { $gt: 50 } }, { $inc: { age: -5 } });
+
+//16 Actualizar el peso de los usuarios que pesan más de 200 libras a 180 libras.
+db.users.updateMany({ weight: { $gt: 200 } }, { $set: { weight: 180 } });
+
+//17 Incrementar el salario de los usuarios que viven en "London" en un 25%.
+db.users.updateMany({ city: "london" }, { $mul: { salary: 1.25 } });
+
+//18 Cambiar el apellido de los usuarios con un salario superior a 4000 dólares a "Smith".
+db.users.updateMany({ salary: { $gt: 4000 } }, { $set: { Lastname: "Smith" } });
+
+//19 Actualizar el correo electrónico de todos los usuarios cuyo nombre empiece por "A" a "nuevo@riwi.es".
+db.users.updateMany({ name: { $regex: /^A/ } }, { $set: { email: "nuevo@riwi.es" } });
+
+//20 Cambiar  la ciudad de residencia de los usuarios con una altura inferior a 160 centímetros a "París".
+db.users.updateMany({ height: { $lt: 160 } }, { $set: { city: "paris" } });
+
+
+
+// Operaciones de Eliminación
+
+//1 Eliminar todos los usuarios que tienen un salario menor que 2000 dólares.
+db.users.deleteMany({ salary: { $lt: 2000 } });
+
+//2 Eliminar usuarios que tienen una edad menor que 25 años.
+db.users.deleteMany({ age: { $lt: 25 } });
+
+//3 Borrar todos los usuarios que viven en "París".
+db.users.deleteMany({ city: "paris" });
+
+//4 Eliminar usuarios que tienen un peso superior a 180 libras.
+db.users.deleteMany({ weight: { $gt: 180 } });
+
+//5 Eliminar usuarios que tienen una altura inferior a 160 centímetros.
+db.users.deleteMany({ height: { $lt: 160 } });
+
+//6 Eliminar todos los usuarios que tienen el nombre "John" y el apellido "Doe".
+db.users.deleteMany({ name: "John", Lastname: "Doe" });
+
+//7 Borrar usuarios que tienen una dirección de correo electrónico específica, por ejemplo, "borrar@riwi.com".
+db.users.deleteMany({ email: "daniel123@gmail.com" });
+
+//8 Eliminar usuarios que no tienen una dirección de correo electrónico registrada.
+db.users.deleteMany({ $or: [{ email: { $exists: false } }, { email: "" }] });
+
+//9 Eliminar usuarios que viven en "Tokyo".
+db.users.deleteMany({ city: "tokyo" });
+
+//10 Borrar todos los usuarios que son menores de 18 años.
+db.users.deleteMany({ age: { $lt: 18 } });
+
+//11 Eliminar usuarios que tienen un salario igual a 0.
+db.users.deleteMany({ salary: 0 });
+
+//12 Borrar usuarios que viven en "New York" y tienen un salario superior a 5000 dólares.
+db.users.deleteMany({ city: "new york", salary: { $gt: 5000 } });
+
+//13 Eliminar usuarios que tienen una dirección de correo electrónico que contiene la palabra "spam".
+db.users.deleteMany({ email: "spam" });
+
+//14 Borrar usuarios que viven en "Bello" y tienen más de 50 años.
+db.users.deleteMany({ city: "bello", age: { $gt: 50 } });
+
+//15 Eliminar todos los usuarios que tienen el apellido "González".
+db.users.deleteMany({ Lastname: "González" });
+
+//16 Borrar usuarios que tienen una edad superior a 70 años.
+db.users.deleteMany({ age: { $gt: 70 } });
+
+//17 Eliminar usuarios que tienen un país de residencia igual a "Canadá" y un salario inferior a 4000 dólares.
+db.users.deleteMany({ country: "canada", salary: { $lt: 4000 } });
+
+//18 Borrar usuarios que tienen un salario entre 1000 y 2000 dólares.
+db.users.deleteMany({ salary: { $gte: 1000, $lte: 2000 } });
+
+//19 Eliminar usuarios que tienen una edad igual a 30 años.
+db.users.deleteMany({ age: 30 });
+
+//20 Borrar usuarios que tienen una altura superior a 190 centímetros.
+db.users.deleteMany({ height: { $gt: 190 } });
+
